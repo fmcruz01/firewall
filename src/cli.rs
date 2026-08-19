cf9f8d7486ebb4f8839b5096926fb938bb0e8ff0
Random <fcruz170@gmail.com>
2026-08-19 13:20:36 +0100

    Arg parse logic to cli.rs.

---
+++ src/cli.rs
@@ -0,0 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "RSCAN", version = "1.0", about = "RSCAN v1.0 -- LAN Scanning Tool", long_about = None)]
+#[command(propagate_version = true)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+    /// Path to the output file. If file does not exist, one will be created
+    #[arg(short, long)]
+    output: Option<String>,
+
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Scans LAN to discover devices
+    Discover {
+        #[arg(short, long, default_value_t = String::from("en0"))]
+        interface: String,
+    },
+    /// Lists all information about device with provided id
+    Fingerprint {
+        /// id of the device to fingerprint
+        id: String,
+    },
+}
+
+pub fn parse() -> Command {
+    Args::parse().command
+}
