59241ed0e16c8367a7380fff8c7fc0aa6f9d4087
Random <fcruz170@gmail.com>
2026-08-19 13:14:17 +0100

    Add cli layout, commands and arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
 
+#[derive(Parser, Debug)]
+#[command(name = "RSCAN", version = "1.0", about = "RSCAN v1.0 -- LAN Scanning Tool", long_about = None)]
+#[command(propagate_version = true)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+    /// Path to the output file. If file does not exist, one will be created
+    #[arg(short, long)]
+    output: Option<String>,
 
-fn main() {}
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Scans LAN network to discover devices
+    Discover {
+        #[arg(short, long, default_value_t = String::from("en0"))]
+        interface: String,
+    },
+    /// Lists all information about device with provided id
+    Fingerprint {
+        /// id of the device to fingerprint
+        id: String,
+    },
+}
+
+fn main() {
+    let args = Args::parse();
+}
